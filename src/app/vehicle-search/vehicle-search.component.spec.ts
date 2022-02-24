935c96b138d8884fee2a86b377d05095c8cac56c
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-24 12:55:23 -0800

    Create search, testing api

---
+++ src/app/vehicle-search/vehicle-search.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VehicleSearchComponent } from './vehicle-search.component';
+
+describe('VehicleSearchComponent', () => {
+  let component: VehicleSearchComponent;
+  let fixture: ComponentFixture<VehicleSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VehicleSearchComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VehicleSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/vehicle-search/vehicle-search.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-vehicle-search',
+  templateUrl: './vehicle-search.component.html',
+  styleUrls: ['./vehicle-search.component.scss']
+})
+export class VehicleSearchComponent implements OnInit {
+  results: any;
+  weather: any;
+  constructor(private http: HttpClient) { }
+
+  Searcher() {
+    console.log(this.results)
+    console.log(this.weather.name)
+    // this.data.forEach((d: any) => {
+    //   console.log(d);
+  // })
+  }
+
+  ngOnInit(): void {
+    this.http.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/5UXWX7C5*BA?format=json`)
+    .subscribe(data => {
+      console.log(data)
+      this.results = data;
+    })
+
+      this.http.get(`http://localhost:9092/api/vehicles`)
+      .subscribe((data:any) => {
+        console.log(data);
+        // this.data = data;
+      })
+
+      this.http
+      .get(`http://api.openweathermap.org/data/2.5/weather?zip=93534,us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial`)
+      .subscribe((response) => {
+        console.log(response);
+        this.weather = 32;
+        console.log(this.weather);
+    });
+
+    // this.weather = 12;
+    console.log(this.weather)
+
+  this.Searcher();
+
+  }
+
+  
+
+}
