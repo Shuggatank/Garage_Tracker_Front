15044771ef74f9be634e4612d8b51107ef9bacd6
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-24 13:52:34 -0800

    API now working

--- src/app/vehicle-search/vehicle-search.component.ts
+++ src/app/vehicle-search/vehicle-search.component.ts
@@ -10,6 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class VehicleSearchComponent implements OnInit {
   results: any;
   weather: any;
+  data: any;
   constructor(private http: HttpClient) { }
 
   Searcher() {
@@ -27,17 +28,17 @@ export class VehicleSearchComponent implements OnInit {
       this.results = data;
     })
 
-      this.http.get(`http://localhost:9092/api/vehicles`)
+    this.http.get(`http://localhost:9092/api/vehicles`)
       .subscribe((data:any) => {
         console.log(data);
-        // this.data = data;
+        this.data = data;
       })
 
-      this.http
+    this.http
       .get(`http://api.openweathermap.org/data/2.5/weather?zip=93534,us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial`)
       .subscribe((response) => {
         console.log(response);
-        this.weather = 32;
+        this.weather = response;
         console.log(this.weather);
     });
 
