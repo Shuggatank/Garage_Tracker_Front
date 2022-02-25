5f6a5d6ecacac8b277ff18b67a841631d51ba7bb
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-25 06:39:40 -0800

    Created and added test tables and cards

---
+++ src/app/garage/garage.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-garage',
+  templateUrl: './garage.component.html',
+  styleUrls: ['./garage.component.scss']
+})
+export class GarageComponent implements OnInit {
+  test: any = [1,2,3,4,5,6,]
+  data: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+    this.http.get(`http://localhost:9092/api/vehicles`)
+    .subscribe((data:any) => {
+      console.log(data);
+      this.data = data;
+    })
+
+
+  }
+
+}
