1bd62a1d182697a1eee675bd071c23e197d5e073
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-24 16:27:12 -0800

    Added Form Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { VehicleSearchComponent } from './vehicle-search/vehicle-search.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
