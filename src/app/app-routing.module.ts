3189a0887b479e9d9dad1fc5b088780cde36c34f
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-25 06:36:58 -0800

    Added garage and vehicle components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './home/home.component';
 import { VehicleSearchComponent} from './vehicle-search/vehicle-search.component';
+import { GarageComponent } from './garage/garage.component';
+import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'search', component: VehicleSearchComponent}
+  {path: 'search', component: VehicleSearchComponent},
+  {path: 'garage', component: GarageComponent},
+  {path: 'vehicle/:id', component: VehicleDetailsComponent}
+ 
 ];
 
 @NgModule({
