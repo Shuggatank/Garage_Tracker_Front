ab2ed475839761d0ef1291d198170618af889461
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-25 06:40:36 -0800

    Created component and added test html

---
+++ src/app/vehicle-details/vehicle-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-details',
+  templateUrl: './vehicle-details.component.html',
+  styleUrls: ['./vehicle-details.component.scss']
+})
+export class VehicleDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
