2f30d6f2ec2bdb31e3be2bb88f9718667dfd5e63
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-24 16:26:34 -0800

    Added routing for search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './home/home.component';
+import { VehicleSearchComponent} from './vehicle-search/vehicle-search.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'search', component: VehicleSearchComponent}
 ];
 
 @NgModule({
