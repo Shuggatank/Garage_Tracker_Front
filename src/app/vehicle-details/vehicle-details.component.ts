11c849e45c362e461e46b86127aff6799d8ce54d
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-25 09:11:12 -0800

    Rendering information from tracker api

--- src/app/vehicle-details/vehicle-details.component.ts
+++ src/app/vehicle-details/vehicle-details.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TrackerapiService } from '../trackerapi.service';
 
 @Component({
   selector: 'app-vehicle-details',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-details.component.scss']
 })
 export class VehicleDetailsComponent implements OnInit {
-
-  constructor() { }
+  vehicle: any;
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params =>{
+      let paramId: string = params.get('id') || 'error';
+      console.log(paramId);
+      this.http.get(`http://localhost:9092/api/vehicles/${paramId}`).subscribe(response => {
+        console.log(response)
+        this.vehicle = response;
+        // if(response.id === par)
+      })
+    })
   }
 
 }
