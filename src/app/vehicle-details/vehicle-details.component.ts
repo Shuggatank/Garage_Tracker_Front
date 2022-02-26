3ed762bffd8da0591106b51119a2a61604b0c350
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-25 20:49:08 -0800

    Updated backen address to Heroku

--- src/app/vehicle-details/vehicle-details.component.ts
+++ src/app/vehicle-details/vehicle-details.component.ts
@@ -10,13 +10,14 @@ import { TrackerapiService } from '../trackerapi.service';
 })
 export class VehicleDetailsComponent implements OnInit {
   vehicle: any;
+  test: any = [1,2,3,4,5,6,]
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params =>{
       let paramId: string = params.get('id') || 'error';
       console.log(paramId);
-      this.http.get(`http://localhost:9092/api/vehicles/${paramId}`).subscribe(response => {
+      this.http.get(`https://garage-tracker.herokuapp.com/api/vehicles/${paramId}`).subscribe(response => {
         console.log(response)
         this.vehicle = response;
         // if(response.id === par)
