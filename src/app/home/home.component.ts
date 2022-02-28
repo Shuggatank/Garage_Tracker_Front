8458904c848e0349a89d5f9274fb10c5ad1402e6
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-27 18:03:57 -0800

    Added template for the progress bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  template: `<ng-progress></ng-progress>`
 })
 export class HomeComponent implements OnInit {
 
