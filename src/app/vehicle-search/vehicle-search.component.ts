00ab756b359ed6d90fcd11ab62b6782b97fb0276
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-27 16:28:56 -0800

    Search working, build form, post data

--- src/app/vehicle-search/vehicle-search.component.ts
+++ src/app/vehicle-search/vehicle-search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, delay } from "rxjs/operators";
-import { FormControl } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-search',
@@ -14,61 +14,46 @@ export class VehicleSearchComponent implements OnInit {
   data: any;
   test: any;
   vin: string = '';
+  form : FormGroup = new FormGroup ({
+    vin: new FormControl(''),
+    make: new FormControl(''),
+    model: new FormControl(''),
+    year: new FormControl(0)
+  })
 
 
  //SAJWJ6BV7G8K32305  //VIN for testing
  //2C3CDZAG9GH249554 //VIN for testing
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private formBuild: FormBuilder) { }
 
-  // Searcher(vin: string) {
-  //   console.log("finding by vin: " + vin)
-  //   console.log(this.results)
-  //   console.log(this.weather.name)
-  //   this.http.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${vin}?format=json`)
-  //   .subscribe(data => {
-  //     console.log(data)
-  //     this.results = data;
-  //   })
-  //   // this.data.forEach((d: any) => {
-  //   //   console.log(d);
-  // // })
-  // }
 
   ngOnInit(): void {
-    // this.http.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/5UXWX7C5*BA?format=json`)
-    // .subscribe(data => {
-    //   console.log(data)
-    //   this.results = data;
-    // })
-    
-    // this.http.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${this.vin}?format=json`)
-    // .subscribe(data => {
-    //   console.log(data)
-    //   this.results = data;
-    // })
-
-    this.test = "working"
-
+    //  this.form = this.formBuild.group({
+    //       vin: new FormControl(''),
+    //       make: new FormControl(''),
+    //       model: new FormControl(''),
+    //       year: new FormControl('')
+    //   })
 
     //Testing backend api
-    this.http.get(`http://localhost:9092/api/vehicles`)
-      .subscribe((data:any) => {
-        console.log(data);
-        this.data = data;
-      })
+    // this.http.get(`http://localhost:9092/api/vehicles`)
+    //   .subscribe((data:any) => {
+    //     console.log(data);
+    //     this.data = data;
+    //   })
 
     //For testing purposes
-    this.http
-      .get(`http://api.openweathermap.org/data/2.5/weather?zip=93534,us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial`)
-      .subscribe((response) => {
-        console.log(response);
-        this.weather = response;
-        console.log(this.weather);
-    });
+    // this.http
+    //   .get(`http://api.openweathermap.org/data/2.5/weather?zip=93534,us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial`)
+    //   .subscribe((response) => {
+    //     console.log(response);
+    //     this.weather = response;
+    //     console.log(this.weather);
+    // });
+    console.log(this.vin)
 
-
-    console.log(this.weather)
+    // console.log(this.weather)
     console.log(this.test)
     // this.Searcher('SAJWJ6BV7G8K32305'); //Works perfectly first time
     // this.vin = 'SAJWJ6BV7G8K32305'; // Undefined
@@ -76,18 +61,37 @@ export class VehicleSearchComponent implements OnInit {
     // this.Searcher((<HTMLInputElement><unknown>document.getElementsByName("VIN")).value); //Undefined on first pass(automatic), passes value on click
     
   }
-  async Searcher(vin: any) {
+  Searcher(vin: any) {
    console.log(vin)
     console.log("finding by vin: " + vin)
+    console.log(this.vin)
 
 
     this.http.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${vin}?format=json`)
       .subscribe(data => {
         console.log(data);
         this.results = data;
+        this.form = this.formBuild.group({
+          vin: new FormControl(vin),
+          make: new FormControl(this.results.Results[6].Value),
+          model: new FormControl(this.results.Results[8].Value),
+          year: new FormControl(this.results.Results[9].Value)
       })
+      console.log(this.form.value)
+  })
 
   }
+  AddVehicle() {
+    console.log("adding a vehicle")
+    console.log(this.results);
+    console.log(this.form.value)
+    const headers = {'content-type': 'application/json'};
+    const body = JSON.stringify(this.form.value)
+    console.log(body)
+    this.http.post<any>(`http://localhost:9092/api/vehicles`, body, {'headers': headers}).subscribe(data => {
+      console.log(data);
+    })
+    this.vin = '';
+  }
   
-
 }
