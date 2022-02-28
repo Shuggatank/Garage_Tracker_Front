9c699083e42277942997f54e15961b4061249a66
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 08:39:56 -0800

    Changed app name for path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/garage-tracker'));
+app.use(express.static(__dirname + '/dist/Garage_Tracker'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/garage-tracker/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Garage_Tracker/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
