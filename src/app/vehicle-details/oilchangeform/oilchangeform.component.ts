0391f8419cb1a9abbfc1f9d517d3bf7db959e2e5
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 12:50:24 -0800

    Moved the date format logic to services

--- src/app/vehicle-details/oilchangeform/oilchangeform.component.ts
+++ src/app/vehicle-details/oilchangeform/oilchangeform.component.ts
@@ -3,100 +3,63 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbCalendar, NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { VehicleDetailsComponent } from '../vehicle-details.component';
+import { CustomadapterService } from '../customadapter.service';
+import { CustomDateParserService } from '../custom-date-parser.service';
 
-/**
- * This Service handles how the date is represented in scripts i.e. ngModel.
- */
- @Injectable()
- export class CustomAdapter extends NgbDateAdapter<string> {
- 
-   readonly DELIMITER = '/';
- 
-   fromModel(value: string | null): NgbDateStruct | null {
-     if (value) {
-       const date = value.split(this.DELIMITER);
-       return {
-         month : parseInt(date[0], 10),
-         day : parseInt(date[1], 10),
-         year : parseInt(date[2], 10)
-       };
-     }
-     return null;
-   }
- 
-   toModel(date: NgbDateStruct | null): string | null {
-     return date ? date.month  + this.DELIMITER + date.day + this.DELIMITER + date.year : null;
-   }
- }
- 
- /**
-  * This Service handles how the date is rendered and parsed from keyboard i.e. in the bound input field.
-  */
- @Injectable()
- export class CustomDateParserFormatter extends NgbDateParserFormatter {
- 
-   readonly DELIMITER = '/';
- 
-   parse(value: string): NgbDateStruct | null {
-     if (value) {
-       const date = value.split(this.DELIMITER);
-       return {
-         month : parseInt(date[0], 10),
-         day : parseInt(date[1], 10),
-         year : parseInt(date[2], 10)
-       };
-     }
-     return null;
-   }
- 
-   format(date: NgbDateStruct | null): string {
-     return date ? date.month + this.DELIMITER + date.day + this.DELIMITER + date.year : '';
-   }
- }
 @Component({
   selector: 'app-oilchangeform',
   templateUrl: './oilchangeform.component.html',
   styleUrls: ['./oilchangeform.component.scss'],
   providers: [
-        {provide: NgbDateAdapter, useClass: CustomAdapter},
-        {provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter}
+        {provide: NgbDateAdapter, useClass: CustomadapterService},
+        {provide: NgbDateParserFormatter, useClass: CustomDateParserService}
       ]
 })
 export class OilchangeformComponent implements OnInit {
   vehicle: any;
   form : FormGroup = new FormGroup ({
     mileage: new FormControl('', [Validators.required]),
-    date: new FormControl(''),
-    oilGrade: new FormControl(''),
-    oilFilter: new FormControl(''),
+    date: new FormControl('', [Validators.required]),
+    oilGrade: new FormControl('', [Validators.required]),
+    oilFilter: new FormControl('', [Validators.required]),
   })
 
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private formBuild: FormBuilder, private ngbCalendar: NgbCalendar, private dateAdapter: NgbDateAdapter<string>) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private formBuild: FormBuilder, private ngbCalendar: NgbCalendar, private dateAdapter: NgbDateAdapter<string>, private details: VehicleDetailsComponent) { }
 
   ngOnInit(): void {
 
     this.form = this.formBuild.group ({
       mileage: new FormControl('', [Validators.required]),
-      date: new FormControl(''),
-      oilGrade: new FormControl(''),
-      oilFilter: new FormControl(''),
+      date: new FormControl('', [Validators.required]),
+      oilGrade: new FormControl('', [Validators.required]),
+      oilFilter: new FormControl('', [Validators.required]),
     })
 
   }
-onSubmit(){
+  onSubmit(){
 
 
-  console.log(this.form.value)
-  const headers = {'content-type': 'application/json'};
-  const body = JSON.stringify(this.form.value)
-  this.route.paramMap.subscribe(params =>{
-    let paramId: string = params.get('id') || 'error';
-    this.http.post<any>(`http://localhost:9092/api/vehicles/${paramId}/oil`, body, {'headers': headers}).subscribe(data => {
-      console.log(data);
+    console.log(this.form.value)
+    const headers = {'content-type': 'application/json'};
+    const body = JSON.stringify(this.form.value)
+    this.route.paramMap.subscribe(params =>{
+      let paramId: string = params.get('id') || 'error';
+      this.http.post<any>(`http://localhost:9092/api/vehicles/${paramId}/oil`, body, {'headers': headers}).subscribe(data => {
+        console.log(data);
+        this.details.getData()
+      })
+
     })
-
-  })
-}
+    
+    
+    this.form = this.formBuild.group ({
+      mileage: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required]),
+      oilGrade: new FormControl('', [Validators.required]),
+      oilFilter: new FormControl('', [Validators.required]),
+    }) 
+  }
 }
 
