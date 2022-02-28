17168190e2f154343a947bed4b3ad9ac580fcdff
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-27 16:18:05 -0800

    Added a resolver for garage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent} from './home/home.component';
 import { VehicleSearchComponent} from './vehicle-search/vehicle-search.component';
 import { GarageComponent } from './garage/garage.component';
 import { VehicleDetailsComponent } from './vehicle-details/vehicle-details.component';
+import { GarageResolver } from './garage.resolver';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'search', component: VehicleSearchComponent},
-  {path: 'garage', component: GarageComponent},
+  {path: 'garage', component: GarageComponent, resolve: {garage: GarageResolver}},
   {path: 'vehicle/:id', component: VehicleDetailsComponent}
  
 ];
