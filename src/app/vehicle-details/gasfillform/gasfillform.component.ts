2c9abce6b95aa7595928cd6b828bab72855b00d8
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-27 21:35:53 -0800

    Created gasFill component

---
+++ src/app/vehicle-details/gasfillform/gasfillform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gasfillform',
+  templateUrl: './gasfillform.component.html',
+  styleUrls: ['./gasfillform.component.scss']
+})
+export class GasfillformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+onSubmit() {}
+}
