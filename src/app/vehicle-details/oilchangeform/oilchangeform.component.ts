9e0471b439d072b800cb8ae6c57c9cd3c03524a9
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 08:49:25 -0800

    Added logic for date format from calendar

--- src/app/vehicle-details/oilchangeform/oilchangeform.component.ts
+++ src/app/vehicle-details/oilchangeform/oilchangeform.component.ts
@@ -1,29 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbCalendar, NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
+/**
+ * This Service handles how the date is represented in scripts i.e. ngModel.
+ */
+ @Injectable()
+ export class CustomAdapter extends NgbDateAdapter<string> {
+ 
+   readonly DELIMITER = '/';
+ 
+   fromModel(value: string | null): NgbDateStruct | null {
+     if (value) {
+       const date = value.split(this.DELIMITER);
+       return {
+         month : parseInt(date[0], 10),
+         day : parseInt(date[1], 10),
+         year : parseInt(date[2], 10)
+       };
+     }
+     return null;
+   }
+ 
+   toModel(date: NgbDateStruct | null): string | null {
+     return date ? date.month  + this.DELIMITER + date.day + this.DELIMITER + date.year : null;
+   }
+ }
+ 
+ /**
+  * This Service handles how the date is rendered and parsed from keyboard i.e. in the bound input field.
+  */
+ @Injectable()
+ export class CustomDateParserFormatter extends NgbDateParserFormatter {
+ 
+   readonly DELIMITER = '/';
+ 
+   parse(value: string): NgbDateStruct | null {
+     if (value) {
+       const date = value.split(this.DELIMITER);
+       return {
+         month : parseInt(date[0], 10),
+         day : parseInt(date[1], 10),
+         year : parseInt(date[2], 10)
+       };
+     }
+     return null;
+   }
+ 
+   format(date: NgbDateStruct | null): string {
+     return date ? date.month + this.DELIMITER + date.day + this.DELIMITER + date.year : '';
+   }
+ }
 @Component({
   selector: 'app-oilchangeform',
   templateUrl: './oilchangeform.component.html',
-  styleUrls: ['./oilchangeform.component.scss']
+  styleUrls: ['./oilchangeform.component.scss'],
+  providers: [
+        {provide: NgbDateAdapter, useClass: CustomAdapter},
+        {provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter}
+      ]
 })
 export class OilchangeformComponent implements OnInit {
   vehicle: any;
   form : FormGroup = new FormGroup ({
-    mileage: new FormControl(''),
+    mileage: new FormControl('', [Validators.required]),
     date: new FormControl(''),
     oilGrade: new FormControl(''),
     oilFilter: new FormControl(''),
   })
 
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private formBuild: FormBuilder) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private formBuild: FormBuilder, private ngbCalendar: NgbCalendar, private dateAdapter: NgbDateAdapter<string>) { }
 
   ngOnInit(): void {
 
     this.form = this.formBuild.group ({
-      mileage: new FormControl(''),
+      mileage: new FormControl('', [Validators.required]),
       date: new FormControl(''),
       oilGrade: new FormControl(''),
       oilFilter: new FormControl(''),
@@ -45,3 +99,4 @@ onSubmit(){
   })
 }
 }
+
