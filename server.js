41f9f52bc158b932c76aa37452734da5af469703
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 14:37:54 -0800

    Updated server.js with proper build name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Garage_Tracker'));
+app.use(express.static('./dist/garage-tracker'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Garage_Tracker/'}),
+    res.sendFile('index.html', {root: 'dist/garage-tracker/'}),
 );
 
 // Start the app by listening on the default Heroku port
