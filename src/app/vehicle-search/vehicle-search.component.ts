71d9052bdf5b7c2452343fbc222d90451b1e1087
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 20:19:40 -0800

    Cleaned up extra code and added comments

--- src/app/vehicle-search/vehicle-search.component.ts
+++ src/app/vehicle-search/vehicle-search.component.ts
@@ -1,66 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, delay } from "rxjs/operators";
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { SearchToastComponent } from './search-toast/search-toast.component'
 
 @Component({
   selector: 'app-vehicle-search',
   templateUrl: './vehicle-search.component.html',
-  styleUrls: ['./vehicle-search.component.scss']
+  styleUrls: ['./vehicle-search.component.scss'],
+  providers: [SearchToastComponent]
 })
 export class VehicleSearchComponent implements OnInit {
   results: any;
-  weather: any;
   data: any;
-  test: any;
   vin: string = '';
   form : FormGroup = new FormGroup ({
-    vin: new FormControl(''),
-    make: new FormControl(''),
-    model: new FormControl(''),
-    year: new FormControl(0)
+    vin: new FormControl('', [Validators.required]),
+    make: new FormControl('', [Validators.required]),
+    model: new FormControl('', [Validators.required]),
+    year: new FormControl(0, [Validators.required])
   })
 
 
  //SAJWJ6BV7G8K32305  //VIN for testing
  //2C3CDZAG9GH249554 //VIN for testing
 
-  constructor(private http: HttpClient, private formBuild: FormBuilder) { }
+  constructor(private http: HttpClient, private formBuild: FormBuilder, private toast: SearchToastComponent) { }
 
 
   ngOnInit(): void {
-
   }
+
+  /* Method for searching by VIN and filling in the form data. If partial data is found(not a full VIN)
+  * then the validators won't let you add the vehicle
+  */
   Searcher(vin: any) {
-   console.log(vin)
-    console.log("finding by vin: " + vin)
-    console.log(this.vin)
-
-
     this.http.get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${vin}?format=json`)
       .subscribe(data => {
-        console.log(data);
         this.results = data;
         this.form = this.formBuild.group({
-          vin: new FormControl(vin),
-          make: new FormControl(this.results.Results[6].Value),
-          model: new FormControl(this.results.Results[8].Value),
-          year: new FormControl(this.results.Results[9].Value)
-      })
-      console.log(this.form.value)
+          vin: new FormControl(vin, [Validators.required]),
+          make: new FormControl(this.results.Results[6].Value, [Validators.required]),
+          model: new FormControl(this.results.Results[8].Value, [Validators.required]),
+          year: new FormControl(this.results.Results[9].Value, [Validators.required])
+        })
+        // If all fields are filled in then the found message is called.
+        if (this.form.value.make != null && this.form.value.model != null && this.form.value.year != null) {
+          this.toast.searchInfo()
+        }
+      
   })
 
   }
   AddVehicle() {
-    console.log("adding a vehicle")
-    console.log(this.results);
-    console.log(this.form.value)
-    const headers = {'content-type': 'application/json'};
-    const body = JSON.stringify(this.form.value)
-    console.log(body)
-    this.http.post<any>(`https://garage-tracker.herokuapp.com/api/vehicles`, body, {'headers': headers}).subscribe(data => {
-      console.log(data);
-    })
+    const headers = {'content-type': 'application/json'}; // Sets up the headers for post
+    const body = JSON.stringify(this.form.value) // Sets up the form data in JSON format to be posted
+    this.toast.openSuccess();
+    this.http.post<any>(`https://garage-tracker.herokuapp.com/api/vehicles`, body, {'headers': headers}).subscribe();
+
+    // Clears all fields
     this.vin = '';
     this.results = '';
 
