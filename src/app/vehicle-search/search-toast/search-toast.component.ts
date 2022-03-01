ad8cfe9ccdfcdf28b1caa51da6d5a25ad5b0446e
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 20:21:01 -0800

    Added code for toast

--- src/app/vehicle-search/search-toast/search-toast.component.ts
+++ src/app/vehicle-search/search-toast/search-toast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-search-toast',
@@ -6,17 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-toast.component.scss']
 })
 export class SearchToastComponent implements OnInit {
-bootstrap: any;
-  constructor() {
-    let bootstrap: any;
-    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-  var toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl)
-  })
+// This component if for holding the logic for the toast notification
+
+
+  constructor(private toast: NgToastService) {
  }
 
   ngOnInit(): void {
+
   }
-  
+  // These are position at the bottom right. If positioned at the top they go under the nav bar.
+  openSuccess() {
+    this.toast.success({detail:"Success",summary:"Vehicle has been added to your garage", duration: 5000, position:'br'})
+    }
+
+  searchInfo() {
+    this.toast.info({detail:"Found",summary:"Matching VIN found", duration: 5000, position:'br'})
+  }
 
 }
