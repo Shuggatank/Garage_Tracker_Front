efed49131ae423d055e4012072badb3d3c9d90c0
Dipesh Patel <dipeshgpatel@gmail.com>
2022-02-28 20:07:41 -0800

    Cleaned up extra code

--- src/app/garage.resolver.ts
+++ src/app/garage.resolver.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
+import { Resolve } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
 import { TrackerapiService } from './trackerapi.service';
 import { catchError } from 'rxjs/operators';
 
@@ -14,8 +9,6 @@ import { catchError } from 'rxjs/operators';
 })
 export class GarageResolver implements Resolve<any> {
   constructor(private tracker: TrackerapiService) {}
-  // resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-  //   return of(true);
   resolve(): Observable<any> {
     return this.tracker.createAPIObservable().pipe(catchError(() => {
       return of('data not available at this time')
